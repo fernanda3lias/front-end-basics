c6ee3eb7fba94ddc25acfa0486bf946b7248c533
fernanda3lias <fernanda.eliasct@gmail.com>
2023-11-11 17:49:16 -0300

    Add organo initial files

---
+++ organo/src/components/Banner/Banner.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+
+function Banner() {
+    return (
+        // JSX
+        <header className="banner">
+            <img src="images/banner.png" alt="Banner principal da pagina do organo."/>
+        </header>
+    )
+}
+export default Banner
